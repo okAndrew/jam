21cca69f04c66d3900122fe30353ee0e7a42cdf5
savruksergiy <savruksergiy@gmail.com>
2013-11-21 09:50:11 -0800

    init 2

---
+++ src/main/java/com/savruksergiy/test5/model/User.java
@@ -0,0 +1,62 @@
+package com.savruksergiy.test5.model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sergiy
+ * Date: 11/21/13
+ * Time: 8:48 AM
+ */
+public class User {
+
+    private long id;
+    private String login;
+    private String email;
+    private String password;
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public long getId() {
+
+        return id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public User() {
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", login='" + login + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+}
--- src/main/webapp/WEB-INF/pages/hello.jsp
+++ src/main/webapp/WEB-INF/pages/hello.jsp
@@ -2,8 +2,10 @@
 
 <html>
 <head>
-    <link rel="stylesheet" type="text/css" href="/spring_test5/resources/css/style.css">
-    <link rel="stylesheet" type="text/css" href="/spring_test5/resources/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="/spring_test5/resources/css/style.css" />
+    <link rel="stylesheet" type="text/css" href="/spring_test5/resources/css/bootstrap.css" />
+    <link rel="stylesheet" type="text/css" href="/spring_test5/resources/css/social-buttons.css" />
+    <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
 </head>
 <body>
 <div id="wrap">
