d09f521ef865762ecddc78cf3b142d1e3c0dac80
Andrew <andrew.kravchuk01@gmail.com>
2014-03-19 21:08:09 +0200

    Added generic-dao and generic-service architecture.

---
+++ src/main/java/com/jar/jam/service/UserService.java
@@ -0,0 +1,7 @@
+package com.jar.jam.service;
+
+import com.jar.jam.domain.model.User;
+
+public interface UserService extends GenericService<User> {
+
+}
---
+++ src/main/java/com/jar/jam/service/impl/GenericServiceImpl.java
@@ -0,0 +1,44 @@
+package com.jar.jam.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.jar.jam.dao.GenericDao;
+import com.jar.jam.service.GenericService;
+
+public class GenericServiceImpl<T> implements GenericService<T> {
+	@Autowired
+	protected GenericDao<T> dao;
+
+	@Override
+	public Long create(T o) {
+		return dao.create(o);
+	}
+
+	@Override
+	public Long saveOrUpdate(T o) {
+		return dao.saveOrUpdate(o);
+	}
+
+	@Override
+	public void update(T object) {
+		dao.update(object);
+	}
+
+	@Override
+	public void delete(Long id) {
+		dao.delete(id);
+	}
+
+	@Override
+	public T get(Long id) {
+		return dao.get(id);
+	}
+
+	@Override
+	public List<T> getAll() {
+		return dao.getAll();
+	}
+
+}
