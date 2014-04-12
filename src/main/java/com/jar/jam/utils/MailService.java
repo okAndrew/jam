d2be4a0426a1f60e3bd52227c0fd5673a0b8cc94
savruksergiy <savruksergiy@gmail.com>
2014-04-12 16:13:15 +0300

    Redesigned registration page

--- src/main/java/com/jar/jam/utils/MailService.java
+++ src/main/java/com/jar/jam/utils/MailService.java
@@ -4,18 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 public class MailService {
 	
-	@Autowired
-	private MailSender ma
-	
-	public void sendMail(String from, String to, String subject, String msg) {
-		 
-		
+//	@Autowired
+//	private MailSender ma
+//	
+//	public void sendMail(String from, String to, String subject, String msg) {
+//		 
+//		
  
 //		message.setFrom(from);
 //		message.setTo(to);
 //		message.setSubject(subject);
 //		message.setText(msg);
 //		mailSender.send(message);	
-	}
+//	}
 	
 }
--- src/main/webapp/resources/js/validation/sign-up-validation.js
+++ src/main/webapp/resources/js/validation/sign-up-validation.js
@@ -1,62 +1,80 @@
-var passedColor = "#01FE10";
-var failedColor = "#FF0000";
 var minLoginLength = 5;
 var minPasswordLength = 6;
-var emailEnable = false;
-var loginEnable = false;
-var passwordEnable = false;
-// var submitButtonEnable = false;
+
+var X_EMAIL_LABEL = "//div[@id='sign-up-email-input']/label";
+var X_LOGIN_LABEL = "//div[@id='sign-up-login-input']/label";
+var X_PASSWORD_LABEL = "//div[@id='sign-up-password-input']/label";
+
+function validateForm() {
+	var checkTerms = document.getElementById('sign-up-terms-checkbox').checked;
+	if (isEmailValid() == true && isLoginValid() == true && isPasswordValid() == true && checkTerms == true) {
+		return true;
+	} else {
+		return false;
+	}
+}
 
 function validateEmail() {
-	var inputField = document.getElementById("email");
-	var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-	if (!regex.test(inputField.value)) {
-		inputField.style.backgroundColor = failedColor;
-		emailEnable = false;
+	var emailBlock = document.getElementById("sign-up-email-input");
+	
+	if (!isEmailValid()) {
+		emailBlock.classList.add("has-error");
+		getElementByXPath(X_EMAIL_LABEL).classList.remove("hide");
 	} else {
-		inputField.style.backgroundColor = passedColor;
-		emailEnable = true;
+		emailBlock.classList.remove("has-error");
+		getElementByXPath(X_EMAIL_LABEL).classList.add("hide");
 	}
-	checkEnableSubmit();
 }
 
 function validateLogin() {
-	var inputField = document.getElementById("login");
-	if (inputField.value.length <= minLoginLength) {
-		inputField.style.backgroundColor = failedColor;
-		loginEnable = false;
+	var loginBlock = document.getElementById("sign-up-login-input");
+	
+	if (!isLoginValid()) {
+		loginBlock.classList.add("has-error");
+		getElementByXPath(X_LOGIN_LABEL).classList.remove("hide");
 	} else {
-		inputField.style.backgroundColor = passedColor;
-		loginEnable = true;
+		loginBlock.classList.remove("has-error");
+		getElementByXPath(X_LOGIN_LABEL).classList.add("hide");
 	}
-	checkEnableSubmit();
 }
 
 function validatePassword() {
-	var inputFieldPassword = document.getElementById("password");
-	var inputFieldPasswordRepeat = document.getElementById("passwordRepeat");
-	if (inputFieldPassword.value.length <= minPasswordLength) {
-		inputFieldPassword.style.backgroundColor = failedColor;
-		passwordEnable = false;
+	var passwordBlock = document.getElementById("sign-up-password-input");
+	
+	if (!isPasswordValid()) {
+		passwordBlock.classList.add("has-error");
+		getElementByXPath(X_PASSWORD_LABEL).classList.remove("hide");
 	} else {
-		inputFieldPassword.style.backgroundColor = passedColor;
+		passwordBlock.classList.remove("has-error");
+		getElementByXPath(X_PASSWORD_LABEL).classList.add("hide");
 	}
-	if (inputFieldPasswordRepeat.value.length > 0
-			&& inputFieldPasswordRepeat.value != inputFieldPassword.value) {
-		inputFieldPasswordRepeat.style.backgroundColor = failedColor;
-	} else if (inputFieldPasswordRepeat.value.length > 0
-			&& inputFieldPasswordRepeat.value == inputFieldPassword.value) {
-		inputFieldPasswordRepeat.style.backgroundColor = passedColor;
-		passwordEnable = true;
-	}
-	checkEnableSubmit();
 };
 
-function checkEnableSubmit() {
-	var submitButton = document.getElementById("signUpButton");
-	if (emailEnable != true || loginEnable != true || passwordEnable != true) {
-		submitButton.disabled = false;
+function isEmailValid() {
+	var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	if (!regex.test(document.getElementById("email").value)) {
+		return false;
 	} else {
-		submitButton.disabled = true;
+		return true;
 	}
 }
+
+function isLoginValid() {
+	if (document.getElementById("login").value.length <= minLoginLength) {
+		return false;
+	} else {
+		return true;
+	}
+}
+
+function isPasswordValid() {
+	if (document.getElementById("password").value.length <= minPasswordLength) {
+		return false;
+	} else {
+		return true;
+	}
+}
+
+function getElementByXPath(path) {
+    return document.evaluate(path, document, null, 9, null).singleNodeValue;
+};
